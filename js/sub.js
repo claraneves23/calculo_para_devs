964e117ccbcf747d1b3ce32e4b769957b5995769
JoaoVale <joaovitorvalec@gmail.com>
2025-03-14 13:56:57 -0300

    botão "voltar ao topo" totalmente finalizado

--- js/sub.js
+++ js/sub.js
@@ -1,3 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let backToTop = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", function () {
+        let scrollPercent = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+
+        if (scrollPercent > 40) {
+            backToTop.style.visibility = "visible";
+            setTimeout(() => {
+                backToTop.style.opacity = "1";
+                backToTop.style.transform = "scale(1)";
+                backToTop.style.boxShadow = "0 0px 5px black"
+            }, 10);
+        } else if(scrollPercent < 40) {
+            backToTop.style.transform = "scale(0.8)";
+            backToTop.style.opacity = "0";
+            setTimeout(() => {
+                backToTop.style.visibility = "hidden";
+            }, 10);
+        }
+    });
+
+    backToTop.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" }); // Faz a rolagem suave ao topo
+    });
+});
+
 let arrow = document.getElementById('arrow');
 let but = document.getElementById('backToTop');
 
@@ -7,26 +34,4 @@ but.addEventListener('mouseenter', () => {
 
 but.addEventListener('mouseleave', () => {
     arrow.classList.remove('arrow_up'); // Remove a animação ao tirar o mouse
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    let backToTop = document.getElementById("backToTop");
-
-    window.addEventListener("scroll", function () {
-        let scrollPercent = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
-
-        if (scrollPercent > 45) {
-            backToTop.style.display = "flex"; // Mostra o botão
-            backToTop.style.opacity = "1";
-        } else {
-            backToTop.style.opacity = "0";
-            setTimeout(() => {
-                backToTop.style.display = "none"; // Esconde após a transição
-            }, 500);
-        }
-    });
-
-    backToTop.addEventListener("click", function () {
-        window.scrollTo({ top: 0, behavior: "smooth" }); // Faz a rolagem suave ao topo
-    });
 });
