71b7be210a4e5211231b3e41747979c116f455d4
JoaoVale <joaovitorvalec@gmail.com>
2025-04-11 14:49:49 -0300

    botão simulados pronto

---
+++ js/sim.js
@@ -0,0 +1,6 @@
+let BtnSim = document.getElementById("BtnSim");
+let SimList = document.getElementById("SimList");
+
+BtnSim.addEventListener("click", function() {
+    SimList.classList.toggle("here");
+});
