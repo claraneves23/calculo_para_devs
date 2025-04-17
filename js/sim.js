7a2074d954d30c3bcbb206004e635e488d17b892
João Vitor Vale <joaovitorvalec@gmail.com>
2025-04-17 18:06:00 -0300

    dropdown testes

--- js/sim.js
+++ js/sim.js
@@ -1,6 +1,20 @@
 let BtnSim = document.getElementById("BtnSim");
+let BtnExc = document.getElementById("BtnExc");
 let SimList = document.getElementById("SimList");
+let ExcList = document.getElementById("ExcList");
 
-BtnSim.addEventListener("click", function() {
-    SimList.classList.toggle("here");
+BtnSim.addEventListener("mouseenter", function() {
+    SimList.classList.add("here");
+});
+
+BtnSim.addEventListener("mouseleave", function() {
+    SimList.classList.remove("here");
+});
+
+BtnExc.addEventListener("mouseenter", function() {
+    ExcList.classList.add("here");
+});
+
+BtnExc.addEventListener("mouseleave", function() {
+    ExcList.classList.remove("here");
 });
